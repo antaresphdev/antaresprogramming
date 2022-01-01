ae73c1ea0fe1afefde02214f93ecd24b9385f3c0
Francis Rubio <francisrubio@protonmail.com>
2022-01-02 05:51:52 +0800

    Donations

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,1 +1,10 @@
 import "./components/Animations";
+
+function copyGCashNumber(textboxID) {
+  const textbox = document.getElementById(textboxID);
+
+  textbox.select();
+  textbox.setSelectionRange(0, 99999);
+
+  document.execCommand("copy");
+}
