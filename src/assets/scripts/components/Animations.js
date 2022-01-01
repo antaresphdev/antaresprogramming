ce20990b48f86feb3b6430f0fd8133b30e1dc5b2
Francis Rubio <francisrubio@protonmail.com>
2022-01-02 00:52:23 +0800

    Table of contents animation

--- src/assets/scripts/components/Animations.js
+++ src/assets/scripts/components/Animations.js
@@ -15,3 +15,40 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 observer.observe(document.querySelector("[data-intersection-trigger]"));
+
+const tocLinks = [...document.querySelectorAll(".toc a")].map((link) => {
+  const hash = new URL(link.href).hash;
+  const target = document.querySelector(hash);
+
+  return { link, target, isActive: false };
+});
+
+tocLinks.forEach((a) => {
+  const observer = new IntersectionObserver((entries) => {
+    const isIntersecting = entries[0].isIntersecting;
+    a.isActive = isIntersecting;
+
+    isIntersecting && renderTOC(tocLinks);
+  });
+
+  observer.observe(a.target);
+});
+
+function renderTOC(links) {
+  const activeElementIndex = links.findIndex((a) => a.isActive);
+  const activeElement =
+    activeElementIndex > -1
+      ? links[activeElementIndex]
+      : { link: document.createElement("a") };
+
+  links.forEach((a, index) => {
+    const li = a.link.closest(".toc li");
+    const ol = a.link.closest(".toc li ol");
+
+    if (index <= activeElementIndex) {
+      li && li.classList.add("active");
+    } else {
+      li && li.classList.remove("active");
+    }
+  });
+}
