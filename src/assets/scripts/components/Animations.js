9826692c2125625748548ecdc06a69f4259572d8
Francis Rubio <francisrubio@protonmail.com>
2022-01-02 00:01:35 +0800

    Navigation bar animation

---
+++ src/assets/scripts/components/Animations.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver((entries) => {
+  const isIntersecting = entries[0].isIntersecting;
+
+  document
+    .querySelector(".navigation-bar")
+    .classList.toggle("bg--primary", !isIntersecting);
+
+  document
+    .querySelector(".navigation-bar ul")
+    .classList.toggle("padding-block--xs", !isIntersecting);
+
+  document
+    .querySelector(".navigation-bar .logo")
+    .classList.toggle("small", !isIntersecting);
+});
+
+observer.observe(document.querySelector("[data-intersection-trigger]"));
