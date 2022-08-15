5bf837a89adf5fb892db1490346c13e0015e0e08
Francis Rubio <francis@antaresph.dev>
2022-08-15 19:27:25 +0800

    Added back navbar animation

--- src/assets/scripts/components/Animations.js
+++ src/assets/scripts/components/Animations.js
@@ -2,20 +2,8 @@ const observer = new IntersectionObserver((entries) => {
   const isIntersecting = entries[0].isIntersecting;
 
   document
-    .querySelector(".navigation-bar")
-    .classList.toggle("bg--primary", !isIntersecting);
-
-  document
-    .querySelector(".navigation-bar")
-    .classList.toggle("shadow", !isIntersecting);
-
-  document
-    .querySelector(".navigation-bar ul")
-    .classList.toggle("padding-block--xs", !isIntersecting);
-
-  document
-    .querySelector(".navigation-bar .logo")
-    .classList.toggle("small", !isIntersecting);
+    .querySelector("[data-navbar]")
+    .classList.toggle("attached", !isIntersecting);
 });
 
 observer.observe(document.querySelector("[data-intersection-trigger]"));
