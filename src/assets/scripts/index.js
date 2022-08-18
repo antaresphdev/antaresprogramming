a7741c21ab4e04a73b2952551cd5298ce74279d0
Francis Rubio <francis@antaresph.dev>
2022-08-18 12:14:29 +0800

    Added donation banner

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -7,29 +7,22 @@ import { TabSystem } from "./components/Tabs"
 const container = $("[data-notification-container]");
 const notifications = new NotificationManager(container);
 
-function copyGCashNumber(textboxID) {
-  const textbox = document.getElementById(textboxID);
+$$("form[data-copy-on-submit]").forEach(form => {
+  const txtCopy = form.querySelector('[data-copy]')
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    txtCopy.select();
+    txtCopy.setSelectionRange(0, 9999);
+    document.execCommand("copy")
 
-  textbox.select();
-  textbox.setSelectionRange(0, 99999);
-
-  document.execCommand("copy");
-
-  notifications.showStatus({
-    title: "Copied!",
-    message: "Nasa clipboard mo na ang kailangan mong info.",
-    feathericon: "check-circle",
-    type: "success",
-  });
-}
-
-$$("button[data-copy]").forEach((button) => {
-  if (button.dataset.copy && button.dataset.copy.length > 0) {
-    button.addEventListener("click", (e) =>
-      copyGCashNumber(button.dataset.copy)
-    );
-  }
-});
+    notifications.showStatus({
+      title: "Copied!",
+      message: "Nasa clipboard mo na ang kailangan mong info.",
+      feathericon: "check-circle",
+      type: "success",
+    });
+  }) 
+})
 
 document.addEventListener("DOMContentLoaded", e => {
   document.documentElement.classList.remove('no-js')
